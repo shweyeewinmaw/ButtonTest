eef25064837d51d6ed1e2c0e082fc214718dc820
shweyeewinmaw <44721761+shweyeewinmaw@users.noreply.github.com>
2022-10-21 11:19:39 +0900

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,14 @@
+
+
+// const open = document.getElementById('open');
+// const modal_container = document.getElementById('modal_container');
+// const close = document.getElementById('close');
+
+// open.addEvetnListener('click',()=>{
+//     modal_container.classList.add('show');
+// });
+
+
+// close.addEvetnListener('click',()=>{
+//     modal_container.classList.remove('show');
+// });
