e0e19d77254f74d9654149f7e0f38b22dd92ff64
Zaw <zawlinnnaing0018@gmail.com>
2022-10-22 20:50:13 +0700

    fix: modal not showing

--- modal.js
+++ modal.js
@@ -1,14 +1,11 @@
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
 
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
 
-// const open = document.getElementById('open');
-// const modal_container = document.getElementById('modal_container');
-// const close = document.getElementById('close');
-
-// open.addEvetnListener('click',()=>{
-//     modal_container.classList.add('show');
-// });
-
-
-// close.addEvetnListener('click',()=>{
-//     modal_container.classList.remove('show');
-// });
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
